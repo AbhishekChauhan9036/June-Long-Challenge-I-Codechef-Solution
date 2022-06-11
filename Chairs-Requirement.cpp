0a1d8011a3db9a2d76bd6f6c2d9db850ae3b5a6f
Abhi <abhishekchauhan9036@gmail.com>
2022-06-11 05:51:57 +0530

    commit mesage

---
+++ Chairs-Requirement.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define RR return 0;
+int32_t main(){
+    int EkdamDoglehoka=1;
+    cin>>EkdamDoglehoka;
+
+    while(EkdamDoglehoka--){
+        int _,__;
+        cin>>_>>__;
+
+        if(_>__){
+            cout<<_-__<<endl;
+        }else{
+            cout<<0<<endl;
+        }
+    }
+    RR
+}
