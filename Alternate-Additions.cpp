03c0b1ed3dedcd6d4394f4d846ffbb2b3a21467b
Abhi <abhishekchauhan9036@gmail.com>
2022-06-11 06:08:45 +0530

    commit mesage

---
+++ Alternate-Additions.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define RR return 0;
+int32_t main(){
+    int EkdamDoglehoka=1,________________=0;
+    cin>>EkdamDoglehoka;
+
+    while(EkdamDoglehoka--){
+        int _,__;
+        cin>>_>>__;
+        ________________=__-_;
+        if(________________%3==2){
+            cout<<"no"<<endl;
+        }else{
+            cout<<"yes"<<endl;
+        }
+    }
+    RR
+}
